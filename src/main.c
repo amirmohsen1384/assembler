eac655291b01800acd15f60972d6b14315dd83a7
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2026-05-25 09:54:36 +0330

    Implemented the file I/O of the program

--- src/main.c
+++ src/main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#define TOTAL_ARGS 3
+#define FILE_NAME_MAX 256
 
 int main(int argc, char **argv)
 {
-    printf("Hello, World.");
+    if (argc < TOTAL_ARGS)
+    {
+        printf("You have not entered the input and output file.");
+        return EXIT_FAILURE;
+    }
+
+    char input_path[FILE_NAME_MAX];
+    strncpy(input_path, argv[1], FILE_NAME_MAX);
+
+    char output_path[FILE_NAME_MAX];
+    strncpy(output_path, argv[2], FILE_NAME_MAX);
+
+    FILE *input_file = fopen(input_path, "r");
+    if (!input_file)
+    {
+        fprintf(stderr, "Failed to open the assembly file. Please try again later.");
+        return EXIT_FAILURE;
+    }
+
+    // Pass the 'input_file' for the first pass
+
+    FILE *output_file = fopen(output_path, "w");
+    if (!output_file)
+    {
+        fprintf(stderr, "Failed to open a file for the output. Please try again later.");
+        return EXIT_FAILURE;
+    }
+
+    // Pass the 'output_file' for the output
+
+    fclose(input_file);
+    fclose(output_file);
+
     return EXIT_SUCCESS;
 }
